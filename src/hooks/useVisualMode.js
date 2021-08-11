5a332058671a98c0dd421dc9418003f7b43a3439
maggiezhao11 <maggiezhao06@hotmail.com>
2021-08-11 17:00:25 -0400

    fix the visual model bugs

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,33 +1,35 @@
 import {useState} from 'react';
 
 export default function useVisualMode(initial) {
-  const [mode, setMode] = useState(initial);
+  // const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
  
     function transition(newMode, replace = false) {
 
       if (replace) {
-        const copiedHistory = [...history]
-        copiedHistory.pop()
-        setMode(newMode);
-        setHistory(prev => ([...prev, mode]))
+        // const copiedHistory = [...history]
+        // copiedHistory.pop()
+        // setMode(newMode);
+        setHistory(prev => ([...prev.slice(0, history.length - 1), newMode]))
 
       } else {
-        setMode(newMode);
-        setHistory(prev => ([...prev, mode]))
+        // setMode(newMode);
+        setHistory(prev => ([...prev, newMode]))
       }
     }
 
     function back() {
-      if(history.length > 1) {
-        const copiedHistory = [...history]
-        setMode(history[history.length - 2])
-        copiedHistory.pop()
-        setHistory(prev => ([...prev, mode]))
+      if(history.length < 2) {
+        return 
       }
+        // const copiedHistory = [...history]
+        // setMode(history[history.length - 2])
+        // copiedHistory.pop()
+        setHistory(prev => ([...prev.slice(0, history.length - 1)]))
+     
     }  
 
 
-  return { mode, transition, back };
+  return { mode: history[history.length - 1], transition, back };
 }
 
