0c531e5dc2a71e65c457a9f27cbebb32f3af1710
maggiezhao11 <maggiezhao06@hotmail.com>
2021-08-11 21:04:40 -0400

    fix some return issues

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -15,7 +15,7 @@ export function getAppointmentsForDay(state, selected_day) {
       if (item === state.appointments[appointment].id) {
         getAppointments.push(state.appointments[appointment]);
       }
-    } return true
+    } return true //add return true for warning
   });
   return getAppointments;
 }
@@ -23,22 +23,22 @@ export function getAppointmentsForDay(state, selected_day) {
 export function getInterviewersForDay(state, selected_day) {
   if (state.days.length <= 0) {
     return [];
-  }
+  } 
   const filteredInterviewers = state.days.filter(day => {
-    if (day.name === selected_day) return true;
+    return day.name === selected_day ? true : false
+    // if (day.name === selected_day) return true; 
   });
   const getInterviewers = [];
   
-  filteredInterviewers.length === 0 ? [] : false;
-  // if (filteredInterviewers.length === 0) {
-  //   return [];
-  // }
+  if (filteredInterviewers.length === 0) {
+    return [];
+  }
   Object.keys(state.interviewers).map(interviewer => {
     for ( let item of filteredInterviewers[0].interviewers)  {
       if (item === state.interviewers[interviewer].id) {
         getInterviewers.push(state.interviewers[interviewer]);
       }
-    } return true
+    } return true //add return true for warning
   });
   return getInterviewers;
 }
@@ -57,28 +57,3 @@ export function getInterview(state, interview) {
   return interviews;
 }
 
-//interview: { student: 'Archie Cohen', interviewer: 2 } } ]
-
-//original one 
-// {
-//   "id":1,
-//   "time":"12pm",
-//   "interview": {
-//     "student": "Lydia Miller-Jones",
-//     "interviewer": {
-//       "id": 1,
-//       "name": "Sylvia Palmer",
-//       "avatar": "https://i.imgur.com/LpaY82x.png"
-//     }
-//   }
-// }
-
-// // transforming one
-// {  
-//   "student": "Lydia Miller-Jones",
-//   "interviewer": {  
-//     "id": 1,
-//     "name": "Sylvia Palmer",
-//     "avatar": "https://i.imgur.com/LpaY82x.png"
-//   }
-// }
