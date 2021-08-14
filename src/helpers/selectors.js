a5c954396e659d6701bb0b0c2d30f956cafb6451
maggiezhao11 <maggiezhao06@hotmail.com>
2021-08-14 13:27:57 -0400

    revise the transition mode for error saving to EMPTY

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -3,15 +3,18 @@ export function getAppointmentsForDay(state, selected_day) {
   if (state.days.length <= 0) {
     return [];
   }
-  const filteredAppointments = state.days.filter(day => {
+  //change array method from filter to find for better practice, no need to use index [0] later.
+  const filteredAppointments = state.days.find(day => {
     return day.name === selected_day ? true : false
   });
   const getAppointments = [];
-  if (filteredAppointments.length === 0) {
+  // if (filteredAppointments.length === 0) { ==> filter version
+  if (filteredAppointments === null) {
     return [];
   }
   Object.keys(state.appointments).map(appointment => { 
-    for ( let item of filteredAppointments[0].appointments)  {
+    // for ( let item of filteredAppointments[0].appointments)  { ==>filter version
+    for ( let item of filteredAppointments.appointments)  {
       if (item === state.appointments[appointment].id) {
         getAppointments.push(state.appointments[appointment]);
       }
@@ -24,16 +27,16 @@ export function getInterviewersForDay(state, selected_day) {
   if (state.days.length <= 0) {
     return [];
   } 
-  const filteredInterviewers = state.days.filter(day => {
+  const filteredInterviewers = state.days.find(day => {
     return day.name === selected_day ? true : false
   });
   const getInterviewers = [];
   
-  if (filteredInterviewers.length === 0) {
+  if (filteredInterviewers === null) {
     return [];
   }
   Object.keys(state.interviewers).map(interviewer => {
-    for ( let item of filteredInterviewers[0].interviewers)  {
+    for ( let item of filteredInterviewers.interviewers)  {
       if (item === state.interviewers[interviewer].id) {
         getInterviewers.push(state.interviewers[interviewer]);
       }
