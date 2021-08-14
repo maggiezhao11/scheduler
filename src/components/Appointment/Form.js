c7ccb4ee541458aec6a8dbae7f6a24cde4c662c6
maggiezhao11 <maggiezhao06@hotmail.com>
2021-08-14 18:28:38 -0400

    add some comments

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -22,6 +22,7 @@ export default function Form(props) {
       setError("Student name cannot be blank");
       return;
     } 
+    //add validation for interviewer input, interviewer cannot be blank
     if (interviewer === null) {
       setError("You haven't chosen any interviewers yet!");
       return;
